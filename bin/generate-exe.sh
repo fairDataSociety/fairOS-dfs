14ddc626f7282b2500dafc4162674d83170030b8
Zahoor Mohamed <zahoor@ethswarm.org>
2020-09-29 17:07:30 +0530

    removed redundant file

--- bin/generate-exe.sh
+++ bin/generate-exe.sh
@@ -1,20 +1,19 @@
 #!/usr/bin/env bash
 
-rm -rf ../dist/*
 package_name=dfs
-platforms=("darwin/386" "darwin/amd64"  "linux/386" "linux/amd64" "linux/arm64" "windows/amd64" "windows/386")
+platforms=("darwin/amd64" "linux/386" "linux/amd64" "linux/arm64" "windows/amd64" "windows/386")
 
 for platform in "${platforms[@]}"
 do
     platform_split=(${platform//\// })
     GOOS=${platform_split[0]}
     GOARCH=${platform_split[1]}
-    output_name=../dist/$package_name'-'$GOOS'-'$GOARCH
+    output_name=./dist/$package_name'-'$GOOS'-'$GOARCH
     if [ $GOOS = "windows" ]; then
         output_name+='.exe'
     fi
 
-    env GOOS=$GOOS GOARCH=$GOARCH go build -o $output_name ../cmd/dfs
+    env GOOS=$GOOS GOARCH=$GOARCH go build -o $output_name ./cmd/dfs
     if [ $? -ne 0 ]; then
         echo 'An error has occurred! Aborting the script execution...'
         exit 1
