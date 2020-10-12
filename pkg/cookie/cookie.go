50cfd294a746eaf4ae57d3beaf1c3d5f26380a77
Zahoor Mohamed <zahoor@ethswarm.org>
2020-10-12 23:37:55 +0530

    set secure as true

--- pkg/cookie/cookie.go
+++ pkg/cookie/cookie.go
@@ -68,7 +68,7 @@ func SetSession(sessionId string, response http.ResponseWriter, cookieDomain str
 			Expires:  expire,
 			HttpOnly: true,
 			SameSite: http.SameSiteNoneMode,
-			//Secure: true,
+			Secure: true,
 			MaxAge: 0, // to make sure that the browser does not persist it in disk
 		}
 	} else {
@@ -80,7 +80,7 @@ func SetSession(sessionId string, response http.ResponseWriter, cookieDomain str
 			HttpOnly: true,
 			Domain:   cookieDomain,
 			SameSite: http.SameSiteNoneMode,
-			//Secure: true,
+			Secure: true,
 			MaxAge: 0, // to make sure that the browser does not persist it in disk
 		}
 	}
