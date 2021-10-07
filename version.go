174fb57898b1a4688d3c566feda3a91fdf909a84
Igor Shadurin <igor.shadurin@gmail.com>
2021-10-07 12:30:05 +0300

    bump version

--- version.go
+++ version.go
@@ -5,7 +5,7 @@
 package dfs
 
 var (
-	version = "0.5.3" // manually set semantic version number
+	version = "0.6.2" // manually set semantic version number
 	commit  string    // automatically set git commit hash
 
 	Version = func() string {
