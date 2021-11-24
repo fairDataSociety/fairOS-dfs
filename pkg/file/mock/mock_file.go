538c042185c52cedfefa71a20dce460526d06e25
asabya <asabya@gmail.com>
2021-11-24 17:36:11 +0530

    fix w/ deepsource recommendation

--- pkg/file/mock/mock_file.go
+++ pkg/file/mock/mock_file.go
@@ -29,34 +29,34 @@ func NewMockFile() *MockFile {
 	return &MockFile{}
 }
 
-func (*MockFile) Upload(_ io.Reader, _ string, _ int64, _ uint32, _, _ string) error {
+func (*MockFile) Upload(fd io.Reader, podFileName string, fileSize int64, blockSize uint32, podPath, compression string) error {
 	return nil
 }
 
-func (*MockFile) Download(_ string) (io.ReadCloser, uint64, error) {
+func (*MockFile) Download(podFileWithPath string) (io.ReadCloser, uint64, error) {
 	return nil, 0, nil
 }
 
-func (*MockFile) ListFiles(_ []string) ([]file.Entry, error) {
+func (*MockFile) ListFiles(files []string) ([]file.Entry, error) {
 	return nil, nil
 }
 
-func (*MockFile) GetStats(_, _ string) (*file.Stats, error) {
+func (*MockFile) GetStats(podName, podFileWithPath string) (*file.Stats, error) {
 	return nil, nil
 }
 
-func (*MockFile) RmFile(_ string) error {
+func (*MockFile) RmFile(podFileWithPath string) error {
 	return nil
 }
 
-func (*MockFile) Read(_ []byte) (n int, err error) {
+func (*MockFile) Read(b []byte) (n int, err error) {
 	return 0, nil
 }
 
-func (*MockFile) AddFileToPath(_, _ string) error {
+func (*MockFile) AddFileToPath(filePath, metaHexRef string) error {
 	return nil
 }
 
-func (*MockFile) LoadFileMeta(_ string) error {
+func (*MockFile) LoadFileMeta(fileNameWithPath string) error {
 	return nil
 }
