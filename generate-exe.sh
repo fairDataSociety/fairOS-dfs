8cb50e158476f5fb1a041eb93147837ef06c4e6c
asabya <asabya@gmail.com>
2022-08-08 09:11:18 +0530

    fix: generate.sh

--- generate-exe.sh
+++ generate-exe.sh
@@ -3,10 +3,10 @@
 package_name=dfs
 cli_package_name=dfs-cli
 platforms=("darwin/amd64" "linux/386" "linux/amd64" "linux/arm64" "windows/amd64" "windows/386")
-COMMIT=`git describe --long --dirty --always --match ""`
+COMMIT=$(git describe --long --dirty --always --match "")
 
 
-echo $COMMIT
+echo "$COMMIT"
 
 
 for platform in "${platforms[@]}"
@@ -16,19 +16,19 @@ do
     GOARCH=${platform_split[1]}
     output_name=./dist/$package_name'-'$GOOS'-'$GOARCH
     cli_output_name=./dist/$cli_package_name'-'$GOOS'-'$GOARCH
-    if [ $GOOS = "windows" ]; then
+    if [ "$GOOS" = "windows" ]; then
         output_name+='.exe'
     fi
 
     echo "generating $output_name"
-    env GOOS=$GOOS GOARCH=$GOARCH  go build -trimpath -ldflags "-s -w -X github.com/fairdatasociety/fairOS-dfs.commit=$COMMIT" -o $output_name ./cmd/dfs
+    env GOOS="$GOOS" GOARCH="$GOARCH"  go build -trimpath -ldflags "-s -w -X github.com/fairdatasociety/fairOS-dfs.commit=$COMMIT" -o $output_name ./cmd/dfs
     if [ $? -ne 0 ]; then
         echo 'An error has occurred! Aborting the script execution...'
         exit 1
     fi
 
     echo "generating $cli_output_name"
-    env GOOS=$GOOS GOARCH=$GOARCH go build -trimpath -ldflags "-s -w -X github.com/fairdatasociety/fairOS-dfs.commit=$COMMIT"  -o $cli_output_name ./cmd/dfs-cli
+    env GOOS="$GOOS" GOARCH="$GOARCH" go build -trimpath -ldflags "-s -w -X github.com/fairdatasociety/fairOS-dfs.commit=$COMMIT"  -o "$cli_output_name" ./cmd/dfs-cli
     if [ $? -ne 0 ]; then
         echo 'An error has occurred! Aborting the script execution...'
         exit 1
