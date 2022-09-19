12044de49c254d7d04ac13d5965a938d43b614bd
Sabyasachi Patra <asabya@gmail.com>
2022-09-19 17:51:01 +0530

    feat: add file seek api, #271 (#272)

--- pkg/file/download.go
+++ pkg/file/download.go
@@ -72,3 +72,40 @@ func (f *File) Download(podFileWithPath string) (io.ReadCloser, uint64, error) {
 	reader := NewReader(fileInode, f.getClient(), meta.Size, meta.BlockSize, meta.Compression, false)
 	return reader, meta.Size, nil
 }
+
+// ReadSeeker does all the validation for the existence of the file and creates a
+// ReadSeekCloser to read the contents of the file from the pod.
+func (f *File) ReadSeeker(podFileWithPath string) (io.ReadSeekCloser, uint64, error) {
+	// check if file present
+	totalFilePath := utils.CombinePathAndFile(podFileWithPath, "")
+	if !f.IsFileAlreadyPresent(totalFilePath) {
+		return nil, 0, ErrFileNotPresent
+	}
+
+	meta := f.GetFromFileMap(totalFilePath)
+	if meta == nil { // skipcq: TCV-001
+		return nil, 0, ErrFileNotFound
+	}
+
+	fileInodeBytes, _, err := f.getClient().DownloadBlob(meta.InodeAddress)
+	if err != nil { // skipcq: TCV-001
+		return nil, 0, err
+	}
+	var fileInode INode
+	err = json.Unmarshal(fileInodeBytes, &fileInode)
+	if err != nil { // skipcq: TCV-001
+		return nil, 0, err
+	}
+
+	//need to change the access time for podFile if it is owned by user
+	if !f.fd.IsReadOnlyFeed() {
+		meta.AccessTime = time.Now().Unix()
+		err = f.updateMeta(meta)
+		if err != nil { // skipcq: TCV-001
+			return nil, 0, err
+		}
+	}
+
+	reader := NewReader(fileInode, f.getClient(), meta.Size, meta.BlockSize, meta.Compression, false)
+	return reader, meta.Size, nil
+}
